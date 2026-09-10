b3bc74788fb4af7afa11f6a43d53fb2aed9edd74
Bernio <jendabernard@gmail.com>
2026-09-10 19:02:47 +0200

    Refresh PWA cache after tracker interaction upgrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tracker-mobile-v1';
+const CACHE = 'tracker-mobile-v2';
 const APP_SHELL = [
   './',
   './index.html',
@@ -22,11 +22,14 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
   );
 });
